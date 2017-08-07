cb7d1a299f57a9dbd3fb8e4864668fd17fff038f
Matt Collins <mdcollins80@gmail.com>
2017-08-07 14:20:53 -0400

    created more game logic

--- js/main.js
+++ js/main.js
@@ -2,21 +2,27 @@ console.log("Up and running!");
 
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
-var cardOne = cards[0];
-var cardTwo = cards[1];
 
-cardsInPlay.push(cardOne);
-cardsInPlay.push(cardTwo);
-
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardTwo);
-
-console.log(cardsInPlay);
-
-if (cardsInPlay.length === 2) {
+var checkForMatch = function () {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
 		alert("You found a match!");
 	} else {
 		alert("Sorry, try again.");
 	}
 }
+
+var flipCard = function (cardId) {
+	console.log("User flipped " + cards[cardId]);
+	cardsInPlay.push(cards[cardId]);
+
+	console.log(cardsInPlay);
+
+	if (cardsInPlay.length === 2) {
+		checkForMatch();
+	}
+
+}
+
+flipCard(0);
+flipCard(2);
+
